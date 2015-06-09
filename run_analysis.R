e117addb2eaa21a0f256e6253f42bb58f5b85514
Leon Duplay <leon.duplay@gmail.com>
2015-06-09 19:21:51 +0200

    well on my way

--- run_analysis.R
+++ run_analysis.R
@@ -21,4 +21,27 @@ subj_train <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FAL
 x_test     <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
 y_test     <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
 subj_test  <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-activities <- read.table("UCI HAR Dataset/activity_labels.txt",header = FALSE)
+
+## Load features and activities
+activities <- read.table("UCI HAR Dataset/activity_labels.txt",header = FALSE,col.names =c("ID","Activity"))
+features   <- read.table("UCI HAR Dataset/features.txt",header = FALSE)
+print("Finished loading data")
+
+## Merge training and test data
+x_merged <- rbind(x_train,x_test)
+y_merged <- rbind(y_train,y_test)
+subj_merged <- rbind (subj_train,subj_test)
+rm(x_train,y_train,subj_train,x_test,y_test,subj_test)
+print("Merged training and test data")
+
+## Replace Activity labels and Features/Metrics in merged data
+features[,2] <- gsub("\\(","",features[,2])
+features[,2] <- gsub("\\)","",features[,2])
+features[,2] <- gsub(",","-",features[,2])
+colnames(x_merged) <- features[,2]
+y_merged <- merge(y_merged,activities, by.x="V1",by.y="ID") 
+y_merged <- select(y_merged,Activity)
+
+## Combine all data and cleanup
+tidy <- cbind(subj_merged, y_merged, x_merged)
+rm(subj_merged, y_merged, x_merged)
